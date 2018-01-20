2ab612c3cfa98abdb0b52c16d27ce51fb5317b66
Evan Porter <evan.porter@dotman.ca>
2018-01-20 19:26:18 -0400

    initial commit

---
+++ prepop.js
@@ -0,0 +1,42 @@
+(function(){
+    "use strict";
+
+    var value = {
+            title: "Turtles",
+            rows: [
+                {
+                    "Turtle Name": "Leonardo",
+                    "Weapon of Choice": "Katana",
+                    "Colour": "Blue",
+                    "Favourite Food": "pizza"
+                },
+                {
+                    "Turtle Name": "Raphael",
+                    "Weapon of Choice": "Sai",
+                    "Colour": "Red",
+                    "Favourite Food": "pizza"
+                },
+                {
+                    "Turtle Name": "Michelangelo",
+                    "Weapon of Choice": "Nunchuku",
+                    "Colour": "Orange",
+                    "Favourite Food": "pizza"
+                },
+                {
+                    "Turtle Name": "Donatello",
+                    "Weapon of Choice": "Bo Staff",
+                    "Colour": "Purple",
+                    "Favourite Food": "pizza"
+                },
+                {
+                    "Turtle Name": "Venus de Milo",
+                    "Weapon of Choice": "Shamanistic Fighting Orbs",
+                    "Colour": "Aqua",
+                    "Favourite Food": "pizza??"
+                },
+            ]
+    };
+
+    var jsonField = document.getElementById("jsonField");
+    jsonField.value = JSON.stringify(value, null, 2);
+})();
---
+++ render.js
@@ -0,0 +1,67 @@
+var RENDER = (function(){
+    "use strict";
+
+    var createTagWithTextNode = function(tagName, text) {
+        var domNode = document.createElement(tagName);
+        domNode.appendChild(document.createTextNode(text));
+        return domNode;
+    };
+
+    var createTableHeadDOM = function(firstRow) {
+        var tHead = document.createElement("thead");
+        var tr = document.createElement("tr");
+
+        Object.keys(firstRow).forEach(function(columnName) {
+            var th = document.createElement("th");
+            th.appendChild(document.createTextNode(columnName));
+            tr.appendChild(th);
+        });
+
+        tHead.appendChild(tr);
+
+        return tHead;
+    };
+
+    var createTableRowDOM = function(row) {
+        var tr = document.createElement("tr");
+
+        Object.keys(row).forEach(function(column) {
+            var td = createTagWithTextNode("td", row[column]);
+            tr.appendChild(td);
+        });
+
+        return tr;
+    }
+
+    var createTableBodyDOM = function(rowData) {
+        var tBody = document.createElement("tbody");
+
+        rowData.forEach(function(row) {
+            tBody.appendChild(createTableRowDOM(row));
+        });
+
+        return tBody;
+    };
+
+    var createTableDOM = function(rowData) {
+        var tableDOM = document.createElement("table");
+
+        tableDOM.appendChild(createTableHeadDOM(rowData[0]));
+        tableDOM.appendChild(createTableBodyDOM(rowData));
+
+        return tableDOM;
+    };
+
+    return function(domId, data){
+        var domObject = document.getElementById(domId);
+        var header = createTagWithTextNode("h1", data.title);
+
+        domObject.appendChild(header);
+        domObject.appendChild(createTableDOM(data.rows));
+    };
+})();
+
+document.getElementById("renderButton").onclick = function(event) {
+    var jsonData = JSON.parse(document.getElementById("jsonField").value);
+    RENDER("replaceMe", jsonData);
+};
